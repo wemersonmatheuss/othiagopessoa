1b37eba9b3c78f9f0eee6084e3aedd775bdd88b8
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-09-12 20:28:39 -0300

    chore: new video

--- src/Components/Depoiments/index.tsx
+++ src/Components/Depoiments/index.tsx
@@ -1,12 +1,13 @@
 import styles from "./styles.module.css"
 
 import depoiment1 from "../../../public/videos/depoiment1.mp4"
+import depoiment2 from "../../../public/videos/IMG_6207.mp4"
 
 export function Depoiments() {
     return (
         <div className={styles.container}>
             <video src={depoiment1} className={styles.video} controls></video>
-            <video src={depoiment1} className={styles.video} controls></video>
+            <video src={depoiment2} className={styles.video} controls></video>
         </div>
     )
 }
